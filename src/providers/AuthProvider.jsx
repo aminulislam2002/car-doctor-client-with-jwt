8d9940aa25dbce02f85a01f802c63259ad984f9a
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-16 14:47:32 -0700

    deploy and hosting to firebase

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -55,7 +55,7 @@ const AuthProvider = ({ children }) => {
           email: currentUser.email,
         };
 
-        fetch("http://localhost:5000/jwt", {
+        fetch("https://car-doctor-server-three-dusky.vercel.app/jwt", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
             <BookService></BookService>
           </PrivateRoute>
         ),
-        loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`),
+        loader: ({ params }) => fetch(`https://car-doctor-server-three-dusky.vercel.app/services/${params.id}`),
       },
       {
         path: "/bookings",
